1d19eefe3d38324fbde2b0d045a8f83da9802fb0
Ujjwal Kumar Singh <ujjwal014658@gmail.com>
2015-05-25 01:49:28 +0530

    Version 2: Track Ride

---
+++ app/src/main/java/in/co/hoi/cabshare/CarDetail.java
@@ -0,0 +1,22 @@
+package in.co.hoi.cabshare;
+
+/**
+ * Created by lenovo pc on 25-05-2015.
+ */
+public class CarDetail {
+    int id;
+    String regNumber;
+    String model;
+    String make;
+    String color;
+
+    public CarDetail(int id, String regnumber, String model, String make, String color){
+        this.id = id;
+        this.regNumber = regnumber;
+        this.model = model;
+        this.make = make;
+        this.color = color;
+
+    }
+
+}
